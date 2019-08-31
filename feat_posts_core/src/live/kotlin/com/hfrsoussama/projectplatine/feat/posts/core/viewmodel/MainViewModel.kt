a25848dc54287109e3a161f1b4f9112f1faf92f5
Oussama HAFFERSSAS <hfrs.oussama@gmail.com>
2019-08-31 17:49:49 +0200

    Added missing try catch blocks for coroutines

--- feat_posts_core/src/live/kotlin/com/hfrsoussama/projectplatine/feat/posts/core/viewmodel/MainViewModel.kt
+++ feat_posts_core/src/live/kotlin/com/hfrsoussama/projectplatine/feat/posts/core/viewmodel/MainViewModel.kt
@@ -11,7 +11,6 @@ import com.hfrsoussama.projectplatine.feat.posts.core.model.presentation.PostUi
 import com.hfrsoussama.projectplatine.feat.posts.core.model.presentation.UserUi
 import com.hfrsoussama.projectplatine.feat.posts.core.model.remote.CommentWs
 import com.hfrsoussama.projectplatine.feat.posts.core.model.remote.PostWs
-import com.hfrsoussama.projectplatine.feat.posts.core.model.remote.UserWs
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import timber.log.Timber
@@ -24,35 +23,33 @@ class MainViewModel : ViewModel() {
 
 
     private val _selectedPost by lazy { MutableLiveData<PostUi>() }
-    val selectedPost : LiveData<PostUi>
+    val selectedPost: LiveData<PostUi>
         get() = _selectedPost
 
 
     private val _selectedPostUser by lazy { MutableLiveData<UserUi>() }
-    val selectedPostUser :LiveData<UserUi>
+    val selectedPostUser: LiveData<UserUi>
         get() = _selectedPostUser
 
 
     private val _selectedPostComments by lazy { MutableLiveData<List<CommentUi>>() }
-    val selectedPostComments : LiveData<List<CommentUi>>
+    val selectedPostComments: LiveData<List<CommentUi>>
         get() = _selectedPostComments
 
 
     init {
         launch {
-            _postsList.value =
-                try {
-                    withContext(Dispatchers.IO) {
-                        PostsRepository().getPosts()
-                            .asSequence()
-                            .map(PostWs::toUiModel)
-                            .toList()
-                    }
-
-                } catch (e: Throwable) {
-                    Timber.e(e)
-                    emptyList()
+            try {
+                _postsList.value = withContext(Dispatchers.IO) {
+                    PostsRepository().getPosts()
+                        .asSequence()
+                        .map(PostWs::toUiModel)
+                        .toList()
                 }
+
+            } catch (e: Throwable) {
+                Timber.e(e)
+            }
 
         }
     }
@@ -65,19 +62,29 @@ class MainViewModel : ViewModel() {
 
     private fun updateSelectedPostComments(post: PostUi) {
         launch {
-            _selectedPostComments.value = withContext(Dispatchers.IO) {
-                PostsRepository().getCommentsByPostId(post.id)
-                    .asSequence()
-                    .map(CommentWs::toUiModel)
-                    .toList()
+            try {
+                _selectedPostComments.value = withContext(Dispatchers.IO) {
+                    PostsRepository().getCommentsByPostId(post.id)
+                        .asSequence()
+                        .map(CommentWs::toUiModel)
+                        .toList()
+                }
+            } catch (e: Throwable) {
+                Timber.e(e)
             }
         }
     }
 
     private fun updateSelectedUser(post: PostUi) {
         launch {
-            _selectedPostUser.value = withContext(Dispatchers.IO) {
-                PostsRepository().getUser(post.userId).toUiModel()
+            try {
+                _selectedPostUser.value =
+                    withContext(Dispatchers.IO) {
+                        PostsRepository().getUser(post.userId).toUiModel()
+                    }
+
+            } catch (e: Throwable) {
+                Timber.e(e)
             }
         }
     }
