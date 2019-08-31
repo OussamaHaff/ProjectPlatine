70adf9cee3cf35b47712bc52a40b4701d1f1f63d
Oussama HAFFERSSAS <hfrs.oussama@gmail.com>
2019-08-31 20:59:14 +0200

    Switched to AppCompat when reading drawable

--- feat_posts_ui/src/main/kotlin/com/hfrsoussama/projectplatine/feat/posts/ui/view/BaseActivity.kt
+++ feat_posts_ui/src/main/kotlin/com/hfrsoussama/projectplatine/feat/posts/ui/view/BaseActivity.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.graphics.Shader
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import androidx.appcompat.content.res.AppCompatResources
 import com.hfrsoussama.projectplatine.feat.posts.ui.R
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -19,7 +19,7 @@ abstract class BaseActivity : AppCompatActivity() {
     }
 
     private fun addBackgroundMosaique(context: Context, backgroundView: View) {
-        val drawable = ContextCompat.getDrawable(context, R.drawable.ic_leaf_pattern)
+        val drawable = AppCompatResources.getDrawable(context, R.drawable.ic_leaf_pattern)
         drawable?.let {
             backgroundView.background = TileVectorDrawable(it, Shader.TileMode.REPEAT)
         }
